914978c09e982f594f74139ec7dc762f863a182d
azaza12345 <61620351+azaza12345@users.noreply.github.com>
2021-09-09 23:46:58 +0300

    Add insertion sort

---
+++ Training/Algorithms/BaseAlgorithms/InsertionSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.BaseAlgorithms
+{
+    public class InsertionSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        public override void Sort()
+        {
+            for(int i = 1; i < Items.Count; i++)
+            {
+                var item = Items[i];
+                var j = i;
+                while (j > 0 && item.CompareTo(Items[j-1]) == -1)
+                {
+                    Items[j] = Items[j - 1];
+                    j--;
+                }
+                Items[j] = item;
+            }
+        }
+    }
+}
---
+++ Training/AlgorithmsTests/BaseAlgorithms/InsertionSortTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.BaseAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.BaseAlgorithms.Tests
+{
+    [TestClass()]
+    public class InsertionSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            var insertion = new InsertionSort<int>();
+            var rnd = new Random();
+            var items = new List<int>();
+
+            for(int i = 0; i < 100; i++)
+            {
+                items.Add(rnd.Next(0, 100));
+            }
+
+            insertion.Items.AddRange(items);
+            insertion.Sort();
+            items.Sort();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(insertion.Items[i], items[i]);
+            }
+        }
+    }
+}
